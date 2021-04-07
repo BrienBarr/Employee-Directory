ea643c2b26dec5e6dfa48729ea2f9e4547857cf2
BrienB <barrdom@hotmail.com>
2021-04-07 19:29:09 -0400

    changed App component to React stateful class component and called axios call with componentDidMount method

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@ import React from 'react';
 // import { Link } from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
+import API from './utils/API'
 import Nav from './components/Nav';
 import Table from './components/Table';
 
-function App() {
-  return (
-    <div className="card text-center">
-      <div className="card-header bg-primary text-white">
-        Employee Directory
-      </div>
-      <Nav />
-      <Table />
-      <footer className="footer footer-dark bg-dark">
-        <div className="container">
-          <strong>Brien Barr</strong><br/>
-          <br/>
-          PH: 317-910-6049
+class App extends React.Component {
+
+  state = {
+    employees: [],
+    error: ""
+  };
+
+  componentDidMount() {
+    API.getEmployees()
+      .then(res => this.setState({ employees: res.data.results }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="card text-center">
+        <div className="card-header bg-primary text-white">
+          Employee Directory
         </div>
-      </footer>
-  </div>
-  );
+        <Nav />
+        <Table />
+        <footer className="footer footer-dark bg-dark">
+          <div className="container">
+            <strong>Brien Barr</strong><br/>
+            <br/>
+            PH: 317-910-6049
+          </div>
+        </footer>
+    </div>
+    );
+  }
 }
 
 export default App;
