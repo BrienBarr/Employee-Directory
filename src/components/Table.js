9303ba9b5cc00a0ab7f40861639b1862a95dc1b1
BrienB <barrdom@hotmail.com>
2021-04-07 23:47:27 -0400

    passed props from App component and mapped through employees to populate rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,35 +1,31 @@
 import React from 'react';
 
-function Table() {
+function Table(props) {
   return (
     <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col"></th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
+            <th scope="col">E-mail</th>
             <th scope="col">Location</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>Houston, TX</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>Indianapolis, IN</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>French Lick, IN</td>
-          </tr>
+          {props.employees.map(
+            item => {
+              return (
+                  <tr key={item.email}>
+                    <td><img src={item.picture.thumbnail} alt={item.name.first + item.name.last} /></td>
+                    <td>{item.name.first}</td>
+                    <td>{item.name.last}</td>
+                    <td>{item.email}</td>
+                    <td>{item.location.city}, {item.location.state}</td>
+                  </tr>
+              )
+            }
+          )}
         </tbody>
       </table>
   )
