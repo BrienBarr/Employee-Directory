9c5856149820c9130313920b6dfd6bee153ca176
BrienB <barrdom@hotmail.com>
2021-04-12 11:50:37 -0400

    changed Nav component to Search

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Search (props) {
+  return(
+    <nav className="navbar navbar-light bg-light d-flex justify-content-center">
+        <form className="form-inline" onSubmit={props.handleFormSubmit}>
+          <input 
+            className="form-control mr-sm-2" 
+            type="search" 
+            placeholder="Search By Name" 
+            aria-label="Search By Name"
+            onChange={props.handleInputChange}
+          />
+          <button 
+            className="btn btn-outline-primary my-2 my-sm-0" 
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </nav>
+  )
+}
+
+export default Search;
