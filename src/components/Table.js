2737a364fbf611f5ceacec826313b0d1837931e2
BrienB <barrdom@hotmail.com>
2021-04-13 01:49:16 -0400

    removed commented out code

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,67 +3,53 @@ import React from 'react';
 function Table(props) {
   const { employees } = props;
   const [sortField, setSortField] = React.useState(null);
-  const [sortDirection, setSortDirection] = React.useState(null);
+  const [sortDirection, setSortDirection] = React.useState('ascending');
   
   let sortedEmployees = [...employees];
 
-    sortedEmployees.sort((a, b) => {
-      let aVal;
-      let bVal;
-      let aVal2;
-      let bVal2;
-      // let direction;
-      switch (sortField){
-        case 'last':
-          aVal = a.name.last;
-          bVal = b.name.last;
-          aVal2 = a.name.first;
-          bVal2 = b.name.first;
-          break;
-        case 'email':
-          aVal = a.email;
-          bVal = b.email;
-          break;
-        case 'location':
-          aVal = a.location.state;
-          bVal = b.location.state;
-          aVal2 = a.location.city;
-          bVal2 = b.location.city;
-          break;
-        default:
-          aVal = a.name.last;
-          bVal = b.name.last;
-          aVal2 = a.name.first;
-          bVal2 = b.name.first;
+  sortedEmployees.sort((a, b) => {
+    let aVal;
+    let bVal;
+    let aVal2;
+    let bVal2;
+
+    switch (sortField){
+      case 'last':
+        aVal = a.name.last;
+        bVal = b.name.last;
+        aVal2 = a.name.first;
+        bVal2 = b.name.first;
+        break;
+      case 'email':
+        aVal = a.email;
+        bVal = b.email;
+        break;
+      case 'location':
+        aVal = a.location.state;
+        bVal = b.location.state;
+        aVal2 = a.location.city;
+        bVal2 = b.location.city;
+        break;
+      default:
+        aVal = a.name.last;
+        bVal = b.name.last;
+        aVal2 = a.name.first;
+        bVal2 = b.name.first;
+    }
+
+    if (aVal < bVal) {
+      return sortDirection === 'ascending' ? -1 : 1;
+    } else if (aVal > bVal) {
+      return sortDirection === 'ascending' ? 1 : -1;
+    } else {
+      if (aVal2 < bVal2){
+        return sortDirection === 'ascending' ? -1 : 1;
+      } else if (aVal2 > bVal2){
+        return sortDirection === 'ascending' ? 1 : -1;
       }
-
-      // switch (sortDirection){
-      //   case 'ascending':
-      //     direction = 'descending';
-      //     break;
-      //   case 'descending':
-      //     direction = 'ascending';
-      //     break;
-      //   default:
-      //     direction = 'ascending';
-      // }
-        
-
-        if (aVal < bVal) {
-          return sortDirection === 'ascending' ? -1 : 1;
-        }
-        else if (aVal > bVal) {
-          return sortDirection === 'ascending' ? 1 : -1;
-        }
-          if (aVal2 < bVal2){
-            return sortDirection === 'ascending' ? -1 : 1;
-          } else if (aVal2 > bVal2){
-            return sortDirection === 'ascending' ? 1 : -1;
-          }
-          return 0;
-    });
-  // }
-  
+      return 0;
+    }
+  });  
   
   return (
     <table className="table">
